240de61403ff9985c3dc60b9de4cbbb843966ddf
ra1d1 <165390850+ra1d1@users.noreply.github.com>
2024-04-05 21:23:03 +0200

    Changed classname and comments to english

--- app/src/main/java/cookbook/backend/logIn.java
+++ app/src/main/java/cookbook/backend/logIn.java
@@ -2,41 +2,35 @@ package cookbook.backend;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.math.BigInteger:
+import java.math.BigInteger;
 
-
-public class logIn {
+public class LogIn {
   private String passwordHash;
 
   public String getPasswordHash() {
     return this.passwordHash;
   }
 
-  // Skapa hashvärde från lösenord, returnera och spara det.
+  // Create a hash value from the password, return and save it.
   public String hashPassword(String password) {
     String hashTxt = "";
-    MessageDigest hash;
     try {
-      hash = MessageDigest.getInstance("SHA1");
+      MessageDigest hash = MessageDigest.getInstance("SHA-1");
       System.out.println("Input password: " + password);
 
-      byte [] MessageDigest = hash.digest(password.getBytes());
-
-      BigInteger num = new BigInteger(1, MessageDigest);
-
-      hashTxt = num.toString(16);
+      byte[] passwordDigest = hash.digest(password.getBytes());
+      BigInteger number = new BigInteger(1, passwordDigest);
+      hashTxt = number.toString(16);
 
       while (hashTxt.length() < 32) {
         hashTxt = "0" + hashTxt;
       }
       this.passwordHash = hashTxt;
-
       return hashTxt;
-    } catch ( NoSuchAlgorithmException e) {
+    } catch (NoSuchAlgorithmException e) {
       e.printStackTrace();
     }
     return hashTxt;
   }
- // Metod här för att ända lösenord till hash och koppla ihop databas för att se om hashvärdet stämmer
-
+  // Method here to change the password to hash and link to the database to check if the hash value matches
 }
