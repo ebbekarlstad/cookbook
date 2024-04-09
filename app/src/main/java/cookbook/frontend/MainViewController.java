d0adf275c7b82228740feccbdb8f946c5e3f6268
Firas Moussa <fm222us@student.lnu.se>
2024-04-09 17:54:32 +0200

    Develop onAction controller that redirects from mainpage to loginpage

--- app/src/main/java/cookbook/frontend/MainViewController.java
+++ app/src/main/java/cookbook/frontend/MainViewController.java
@@ -1,15 +1,22 @@
 package cookbook.frontend;
 
-import cookbook.backend.DatabaseMng;
-import java.sql.Connection;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
+/* import cookbook.backend.DatabaseMng;
+import java.sql.Connection; */
+
 
 public class MainViewController {
   @FXML
   private Label mysqlStatus;
 
-  @FXML
+/*   @FXML
   private void initialize() {
     // Attempt to get a database connection
     Connection conn = DatabaseMng.getConnection();
@@ -19,5 +26,21 @@ public class MainViewController {
         } else {
             mysqlStatus.setText("An error has occurred: " + DatabaseMng.getLastErrorMessage());
         }
+  } */
+
+  @FXML
+  private void handleLoginButton(ActionEvent event) {
+    try {
+      //Load the login page FXML
+      Parent loginPageParent = FXMLLoader.load(getClass().getResource("/LoginView.fxml"));
+      Scene loginPageScene = new Scene(loginPageParent);
+
+      // Get the current stage and replace it
+      Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+      window.setScene(loginPageScene);
+      window.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
