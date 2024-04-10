2af85361c0dfdaab2b9d74694df3e348888f8e23
Anas Adiel <aa226ws@student.lnu.se>
2024-04-10 19:01:01 +0200

    Fixed the add and remove functionality to the Reipe view

--- app/src/main/java/cookbook/frontend/RecipeViewController.java
+++ app/src/main/java/cookbook/frontend/RecipeViewController.java
@@ -2,21 +2,25 @@ package cookbook.frontend;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class RecipeViewController {
 
     @FXML
-    private ListView<?> ListOfRecipe;
+    private TextField name;
+
+    @FXML
+    private ListView<String> ListOfRecipe;
 
     @FXML
     void addRecipe(MouseEvent event) {
-
+        ListOfRecipe.getItems().add(name.getText());
     }
 
     @FXML
     void removeRecipe(MouseEvent event) {
-
+        ListOfRecipe.getItems().remove(name.getText());
     }
 
 }
