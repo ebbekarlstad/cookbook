17e739db2c7342b2297e6d7224bc2ea1c84ac4b0
as226pr <as226pr@student.lnu.se>
2024-04-16 00:54:59 +0200

    change the text area to a Lable instead pf text

--- app/src/main/java/cookbook/frontend/fe_controllers/RecipeDetailController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/RecipeDetailController.java
@@ -1,17 +1,11 @@
-package cookbook.frontend.fe_controllers;
-
-import cookbook.backend.be_objects.CookingOB;
-import javafx.scene.control.TextArea;
-import javafx.fxml.FXML;
-
 public class RecipeDetailController {
   @FXML
-  private TextArea ingredientsText;
+  private Label ingredientsText;
   @FXML
-  private TextArea descriptionText;
+  private Label descriptionText;
 
   public void setRecipeDetails(CookingOB recipe) {
-      ingredientsText.setText(recipe.getIngredients());
-      descriptionText.setText(recipe.getName());
+      ingredientsText.setText("Ingredients: \n" + recipe.getIngredients());
+      descriptionText.setText("Description: \n" + recipe.getName());
   }
 }
