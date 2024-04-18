56a6e90bf5fe7699c1daf5dcb485db796de66767
Firas Moussa <fm222us@student.lnu.se>
2024-04-18 14:10:03 +0200

    Add errorLabel for login to display login status

--- app/src/main/java/cookbook/frontend/fe_controllers/LoginViewController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/LoginViewController.java
@@ -4,8 +4,10 @@ import cookbook.backend.be_objects.LogIn;
 import cookbook.backend.DatabaseMng;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 
 public class LoginViewController {
@@ -17,12 +19,16 @@ public class LoginViewController {
   private PasswordField passwordField;
 
   @FXML
+  private Label errorLabel;
+
+  @FXML
   private Button loadNavButton;
 
   private DatabaseMng dbManager = new DatabaseMng();
 
   @FXML
   private void handleLoginButton(ActionEvent event) {
+    errorLabel.setText("");
     String username = usernameField.getText();
     String password = passwordField.getText();
 
@@ -31,8 +37,13 @@ public class LoginViewController {
 
     if (login.doLogin(username, password)) {
       System.out.println("Login successful!");
+      errorLabel.setTextFill(Color.GREEN);
+      errorLabel.setText("Login successful!");
+      
     } else {
       System.out.println("Login failed!");
+      errorLabel.setTextFill(Color.RED);
+      errorLabel.setText("Invalid username or password.");
     }
   } 
 }
