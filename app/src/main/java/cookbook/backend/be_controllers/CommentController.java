777a517cdc70f7c6f5b5abebcc656f38c428c2cd
Kinan Almasri <ka223mf@student.lnu.se>
2024-04-18 11:20:50 +0200

    Change tab size

--- app/src/main/java/cookbook/backend/be_controllers/CommentController.java
+++ app/src/main/java/cookbook/backend/be_controllers/CommentController.java
@@ -1,5 +1,28 @@
 package cookbook.backend.be_controllers;
 
+import java.sql.Connection;
+
+import cookbook.backend.DatabaseMng;
+import cookbook.backend.be_objects.CommentObject;
 public class CommentController {
-    
+
+  public boolean addComment(){
+    String sql = "INSERT INTO users (recipeId, userId, text, timestamp) VALUES (?, ?, ?, ?)";
+      try (Connection conn = DatabaseMng.getConnection(); 
+      PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+      pstmt.setString(1, CommentObject.getRecipeId());
+      pstmt.setString(2, CommentObject.getUserId());
+      pstmt.setString(3, CommentObject.getText());
+      pstmt.setBoolean(4,CommentObject.getTimestamp());
+
+
+
+
+
+    }
+
+
+}
 }
--- app/src/main/java/cookbook/backend/be_objects/CommentObject.java
+++ app/src/main/java/cookbook/backend/be_objects/CommentObject.java
@@ -2,81 +2,49 @@ package cookbook.backend.be_objects;
 
 
 public class CommentObject {
-    private int commentId;
-    private int recipeId;
-    private int userId;
-    private String text;
-    private String timestamp;
+  private int commentId;
+  private int recipeId;
+  private int userId;
+  private String text;
+  private String timestamp;
 
-
-
-    public CommentObject(int commentId, int recipeId, int userId, String text, String timestamp) {
-        this.commentId = commentId;
-        this.recipeId = recipeId;
-        this.userId = userId;
-        this.text = text;
-        this.timestamp = timestamp;
-    }
-
-
-
-    public int getCommentId() {
-        return commentId;
-    }
-
-
-
-    public void setCommentId(int commentId) {
-        this.commentId = commentId;
-    }
-
-
-
-    public int getRecipeId() {
-        return recipeId;
-    }
-
-
-
-    public void setRecipeId(int recipeId) {
-        this.recipeId = recipeId;
-    }
-
-
-
-    public int getUserId() {
-        return userId;
-    }
-
-
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-
-
-    public String getText() {
-        return text;
-    }
-
-
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
+  public CommentObject(int commentId, int recipeId, int userId, String text, String timestamp) {
+      this.commentId = commentId;
+      this.recipeId = recipeId;
+      this.userId = userId;
+      this.text = text;
+      this.timestamp = timestamp;
+  }
+  public int getCommentId() {
+      return commentId;
+  }
+  public void setCommentId(int commentId) {
+      this.commentId = commentId;
+  }
+  public int getRecipeId() {
+      return recipeId;
+  }
+  public void setRecipeId(int recipeId) {
+      this.recipeId = recipeId;
+  }
+  public int getUserId() {
+      return userId;
+  }
+  public void setUserId(int userId) {
+      this.userId = userId;
+  }
+  public String getText() {
+      return text;
+  }
+  public void setText(String text) {
+      this.text = text;
+  }
+  public String getTimestamp() {
+      return timestamp;
+  }
+  public void setTimestamp(String timestamp) {
+      this.timestamp = timestamp;
+  }
 
     
 
