bf603c501c8950e230575e1c0bc539aca4c0285a
Ebbe <ek224ev@student.lnu.se>
2024-04-18 13:57:46 +0200

    warning fix

--- app/src/main/java/cookbook/frontend/fe_controllers/RecipeViewController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/RecipeViewController.java
@@ -11,10 +11,10 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.stage.Stage;
 
 /* import javafx.scene.input.MouseEvent; */
 
