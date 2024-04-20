b1fe552e0103e7f5a1f6223bbf8c485dd388e478
rd222ki <rd222ki@student.lnu.se>
2024-04-20 21:48:43 +0200

    add and remove favorite recipe, backend-controller

--- app/src/main/java/cookbook/backend/be_controllers/UserController.java
+++ app/src/main/java/cookbook/backend/be_controllers/UserController.java
@@ -1,9 +1,13 @@
 package cookbook.backend.be_controllers;
 import cookbook.backend.be_objects.User;
+import cookbook.backend.be_objects.CookingOB;
 import cookbook.backend.DatabaseMng;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserController {
   private DatabaseMng dbManager;
@@ -38,6 +42,50 @@ public class UserController {
     } catch (SQLException e) {
         System.err.println("Database error during user insertion: " + e.getMessage());
         return false;
+    }
+  }
+
+  public List<CookingOB> getFavoriteRecipes() {
+    List<CookingOB> recipes = new ArrayList<>();
+    String sql = "SELECT RecipeName, Ingredients FROM favorite_recipes WHERE UserName = ?";
+    try (Connection conn = dbManager.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        
+      pstmt.setString(1, user.getUserName());
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+        String recipeName = rs.getString("RecipeName");
+        String ingredients = rs.getString("Ingredients");
+        recipes.add(new CookingOB(ingredients, recipeName));
+      }
+      return recipes;
+    } catch (SQLException e) {
+      System.err.println("Database error during fetching favorite recipes: " + e.getMessage());
+      return null;
+    }
+  }
+
+  
+  public boolean removeFavoriteRecipe(CookingOB recipe) {
+    String sql = "INSERT INTO favorite:recipes (UserName, RecipeName, Ingredients) VALUES (?, ?, ?)";
+    try (Connection conn = dbManager.getConnection());
+      PreparedStatement pstmt = conn.prepareStatement(sql)) {
+      
+      pstmt.setString(1, user.getUserName());
+      pstmt.setString(2, recipe.getName());
+      pstmt.setString(3, recipe.getIngredients());
+      int affectedRows = pstmt.executeUpdate();
+      if (affectedRows > 0) {
+      user.removeFavoriteRecipe(recipe);
+      System.out.println("Favorite recipe removed successfully.");
+      return true;
+      } else {
+        System.out.println("No rows affected.");
+        return false;
+      }
+    } catch (SQLException e) {
+      System.err.println("Database error during removing favorite recipe: " + e.getMessage());
+      return false;
     }
   }
 }
