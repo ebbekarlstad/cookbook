272b33fe70fea598971a418870467ae55010d5ce
rd222ki <rd222ki@student.lnu.se>
2024-04-30 13:26:29 +0200

    started on weekly recipe list controller

---
+++ app/src/main/java/cookbook/backend/be_controllers/WeeklyController.java
@@ -0,0 +1,14 @@
+package cookbook.backend.be_controllers;
+import cookbook.backend.DatabaseMng;
+import cookbook.backend.be_objects.Recipe;
+import cookbook.backend.be_objects.User;
+
+public class WeeklyController {
+  private DatabaseMng dbManager;
+
+  public WeeklyController(DatabaseMng dbManager) {
+    this.dbManager = dbManager;
+  }
+
+  
+}
