6ef150ebd26ea0327eebd1e99f10fc964010d532
rd222ki <rd222ki@student.lnu.se>
2024-04-30 14:32:27 +0200

    working on method for weekdays in list

--- app/src/main/java/cookbook/backend/be_controllers/WeeklyController.java
+++ app/src/main/java/cookbook/backend/be_controllers/WeeklyController.java
@@ -1,7 +1,15 @@
 package cookbook.backend.be_controllers;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import cookbook.backend.DatabaseMng;
 import cookbook.backend.be_objects.Recipe;
 import cookbook.backend.be_objects.User;
+
+import java.sql.Date;
+import java.util.List;
 
 public class WeeklyController {
   private DatabaseMng dbManager;
@@ -10,5 +18,23 @@ public class WeeklyController {
     this.dbManager = dbManager;
   }
 
-  
+  public void createWeeklyList(int userId, List<String> recipeIds) {
+    String insertWeeklyListSQL = "INSERT INTO weekly_dinner_list (UserID, Week) VALUES (?, ?)";
+    String insertDinnerListRecipeSQL = "INSET INTO dinner_list_recipes (WeeklyDinnerListID, RecipeID, DayOfWeek) VALUES (?, ?, ?)";
+
+    try (Connection conn = dbManager.getConnection();
+        PreparedStatement pstmtWeeklyList = conn.prepareStatement(insertWeeklyListSQL);
+        PreparedStatement pstmtWeeklyDinner = conn.prepareStatement(insertDinnerListRecipeSQL)) {
+        
+        pstmtWeeklyList.setInt(1, userId);
+        pstmtWeeklyList.setDate(1, new Date(System.currentTimeMillis()));
+        pstmtWeeklyList.executeUpdate();
+        
+        // ResultSet och Map för veckodagarna
+        }
+
+    }
+  }
+
+
 }
