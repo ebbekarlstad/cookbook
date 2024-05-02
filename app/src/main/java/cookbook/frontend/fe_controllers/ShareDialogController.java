6d8ba13df4032ee92b0e211eb83efc0775867c92
Ebbe <ek224ev@student.lnu.se>
2024-05-02 14:36:29 +0200

    added method for saving a message to database

--- app/src/main/java/cookbook/frontend/fe_controllers/ShareDialogController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/ShareDialogController.java
@@ -12,6 +12,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import cookbook.backend.DatabaseMng;
 
+import cookbook.backend.be_objects.Message;
+import cookbook.backend.be_controllers.MessageController;
+
 public class ShareDialogController {
   
   private DatabaseMng dbManager;
@@ -21,11 +24,36 @@ public class ShareDialogController {
   }
 
   @FXML
-  private ComboBox<User> recipientComboBox; 
+  private ComboBox<User> recipientComboBox;
+  
+  private Long senderId;
+  private Long receiverId;
+  private String recipeId;
+  private String content; 
 
   @FXML
   public void sendShare(ActionEvent event) {
-    // Method implementation
+    User recipient = recipientComboBox.getValue();
+    if (recipient != null) {
+      Message message = new Message(this.senderId, this.receiverId, this.recipeId, this.content);
+      
+      message.setMessageId((long) 1);
+      message.setSenderId((long) 1);
+      message.setReceiverId((long) 2);
+      message.setRecipeId("1");
+      message.setContent("This is the message content");
+      message.setSentTime(new java.sql.Timestamp(new java.util.Date().getTime()));
+
+      MessageController messageManager = new MessageController(dbManager);
+      boolean result = messageManager.saveMessage(message);
+      if (result) {
+          System.out.println("Message sent successfully!");
+      } else {
+          System.out.println("Failed to send message.");
+      }
+    } else {
+      System.out.println("No recipient selected.");
+    }
   }
 
   @FXML
