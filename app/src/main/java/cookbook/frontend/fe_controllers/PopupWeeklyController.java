54702d8e44386b5b9d6a76b6abbe77f627a63321
rd222ki <rd222ki@student.lnu.se>
2024-05-04 15:08:21 +0200

    starting on controller for popupwindow

---
+++ app/src/main/java/cookbook/frontend/fe_controllers/PopupWeeklyController.java
@@ -0,0 +1,26 @@
+package cookbook.frontend.fe_controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.sql.Date;
+
+import cookbook.backend.DatabaseMng;
+import cookbook.backend.be_controllers.WeeklyController;
+
+public class PopupWeeklyController {
+
+    @FXML
+    private ComboBox<String> weeksComboBox;
+    @FXML
+    private ComboBox<String> daysComboBox;
+
+    private WeeklyController weeklyController;
+    private int userId = 1;
+
+
+    
+}
--- app/src/main/java/cookbook/frontend/fe_controllers/PopupWeelyController.java
+++
@@ -1,5 +0,0 @@
-package cookbook.frontend.fe_controllers;
-
-public class PopupWeelyController {
-    
-}
