06cb570c7c3cc1c186f1dbc86b679ecebbee1191
Ebbe <ek224ev@student.lnu.se>
2024-05-04 13:56:44 +0200

    added icon

--- app/src/main/java/cookbook/frontend/UIHandler.java
+++ app/src/main/java/cookbook/frontend/UIHandler.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class UIHandler extends Application {
@@ -12,6 +13,10 @@ public class UIHandler extends Application {
   public void start(Stage primaryStage) throws Exception {
     // Load the FXML layout
     Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
+
+    // Set the application icon
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+
     // Set the scene and show the stage
     primaryStage.setScene(new Scene(root, 934, 703));
     primaryStage.setTitle("Cookbook");
