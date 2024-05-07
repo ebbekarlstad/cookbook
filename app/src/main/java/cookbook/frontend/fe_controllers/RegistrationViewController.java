b48ac921a53d82b89011e283917c01ae776b5747
Firas Moussa <fm222us@student.lnu.se>
2024-05-07 23:00:24 +0200

    Add FXML variables in registrationviewcontroller

--- app/src/main/java/cookbook/frontend/fe_controllers/RegistrationViewController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/RegistrationViewController.java
@@ -1,7 +1,10 @@
 package cookbook.frontend.fe_controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 
 public class RegistrationViewController {
@@ -19,8 +22,26 @@ public class RegistrationViewController {
   private TextField usernameField;
 
   /**
-   * DisplayName field
+   * DisplayName field.
    */
   @FXML
   private TextField displayNameField;
+
+  /**
+   * Progress indicator that runs during the registration process in backend.
+   */
+  @FXML
+  private ProgressIndicator progressCircle;
+
+  /**
+   * Error label that displays a message in case of any error in registration.
+   */
+  @FXML
+  private Label errorLabel;
+
+  /**
+   * Registration button that takes input and connects it to the backend methods.
+   */
+  @FXML
+  private Button registerButton;
 }
