4bbeaac8a688559956d72447c6aa00055487e113
Firas Moussa <fm222us@student.lnu.se>
2024-05-07 23:43:17 +0200

    Initial method to register user in database on buttonclick

--- app/src/main/java/cookbook/frontend/fe_controllers/RegistrationViewController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/RegistrationViewController.java
@@ -1,11 +1,20 @@
 package cookbook.frontend.fe_controllers;
 
+import cookbook.backend.be_controllers.UserController;
+import cookbook.backend.be_objects.User;
+
+import java.sql.SQLException;
+
+import cookbook.backend.DatabaseMng;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 public class RegistrationViewController {
 
@@ -44,4 +53,66 @@ public class RegistrationViewController {
    */
   @FXML
   private Button registerButton;
+
+  private UserController userController;
+  private DatabaseMng dbManager;
+
+  public RegistrationViewController() throws SQLException {
+    this.dbManager = new DatabaseMng();
+    this.userController = new UserController(dbManager);
+}
+
+  @FXML
+  private void registerUser(ActionEvent event) {
+
+        // Empty the errorlabel
+    errorLabel.setText("");
+
+    // Get the input from the username & password field.
+    String username = usernameField.getText();
+    String displayname = displayNameField.getText();
+    String password = passwordField.getText();
+
+    // Show progress indicator
+    progressCircle.setVisible(true);
+
+    /**
+     * Boolean that initializes a new Task object.
+     * @return False if credentials are wrong. True if correct.
+     */
+    Task<Boolean> registerTask = new Task<>() {
+      @Override
+      protected Boolean call() throws Exception {
+        Thread.sleep(2000);  // Sleep for 2 seconds
+        User newUser = new User(null, username, displayname, password, null, dbManager, password);
+        userController.setUser(newUser);
+        return userController.saveToDatabase();
+      }
+    };
+    
+
+    // If login succeded
+    registerTask.setOnSucceeded(e -> {
+      progressCircle.setVisible(false);
+
+      if (registerTask.getValue()) {
+        errorLabel.setTextFill(Color.GREEN);
+        System.out.println("Registration Successful!");
+        errorLabel.setText("Registration Successful!");
+        //loadNavigationView(event);
+
+      } else {
+        errorLabel.setTextFill(Color.RED);
+        errorLabel.setText("Registration Failed.");
+      }
+    });
+
+    registerTask.setOnFailed(e -> {
+      progressCircle.setVisible(false);
+      errorLabel.setTextFill(Color.RED);
+      errorLabel.setText("Error during registration.");
+    });
+    // Start login task
+    new Thread(registerTask).start();
+  }
 }
