a3e57fec7c7a938c68a5df62c341733bc3e3522f
as226pr <as226pr@student.lnu.se>
2024-05-07 16:44:30 +0200

    initialize the list in the tutorial page

--- app/src/main/java/cookbook/frontend/fe_controllers/SearchHelpController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/SearchHelpController.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import cookbook.backend.be_objects.HelpMain;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class SearchHelpController {
@@ -21,23 +25,29 @@ public class SearchHelpController {
   @FXML
   private TextArea helpResultField;
   @FXML
+  private ListView<String> listTutorials;
+  @FXML
   private Button back;
+
+  private List<HelpMain> helpTopics;
 
   @FXML
   private void initialize() {
     initializeHelpText();
+    loadListView();
   }
 
-private void initializeHelpText() {
-    List<HelpMain> helpTopics = Arrays.asList(
+  private void initializeHelpText() {
+    helpTopics = Arrays.asList(
         new HelpMain("How to add a new recipe?",
-            "To add a new recipe, click on the 'Add Recipe' option from the main menu, which will navigate you to the adding recipe menu. You will then be able to provide all the relevant information for the desired recipe you want to add. When done, click on 'Add Recipe' to save the recipe you added to your cookbook."),
+            "To add a new recipe, click on the 'Add Recipe' option from the main menu..."),
         new HelpMain("How do I send a recipe to another person?",
-            "To send a recipe: Click on the recipe you would like to send, then click the Mail icon on the bottom. Then, click on the user you would like to send the recipe to, and fill in an optional message. Once done, click 'Send' and you are finished."),
+            "To send a recipe: Click on the recipe you would like to send, then click the Mail icon..."),
         new HelpMain("How do I check my messages?",
-            "To check your messages: Click the 'Messages' icon. This takes you to your messages menu. Click the user you'd like to see your messages from, which shows your inbox from them.")
-        // Add all other help topics similarly
+            "To check your messages: Click the 'Messages' icon...")
+        // Add other topics similarly
     );
+
     StringBuilder helpTextBuilder = new StringBuilder();
     helpTextBuilder.append("## **Welcome to the Digital Cookbook Help System**\n\n");
     for (HelpMain topic : helpTopics) {
@@ -45,7 +55,9 @@ private void initializeHelpText() {
                        .append(topic.getDescription()).append("\n\n");
     }
     helpResultField.setText(helpTextBuilder.toString());
-}
+  }
+
+
 
 
 
@@ -53,7 +65,7 @@ private void initializeHelpText() {
   private void searchHelp() {
     String keyword = searchByNameField.getText().trim().toLowerCase();
     String content = helpResultField.getText().toLowerCase();
-    int startIndex = content.indexOf(keyword);
+    int startIndex = content.indexOf("## **" + keyword);
     if (startIndex != -1) {
       helpResultField.selectRange(startIndex, startIndex + keyword.length());
       helpResultField.requestFocus();
