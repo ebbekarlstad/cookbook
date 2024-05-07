75f9bfe3357e2e442b1022337eefda2ce20a2bac
Firas Moussa <fm222us@student.lnu.se>
2024-05-07 22:54:18 +0200

    Initialize RegistrationViewController

---
+++ app/src/main/java/cookbook/frontend/fe_controllers/RegistrationViewController.java
@@ -0,0 +1,26 @@
+package cookbook.frontend.fe_controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegistrationViewController {
+
+    /**
+   * Password Field.
+   */
+  @FXML
+  private PasswordField passwordField;
+  
+  /**
+   * Username Field.
+   */
+  @FXML
+  private TextField usernameField;
+
+  /**
+   * DisplayName field
+   */
+  @FXML
+  private TextField displayNameField;
+}
