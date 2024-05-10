9743a5995455fcc3dab848bcd4da89df6e6f076f
Firas Moussa <fm222us@student.lnu.se>
2024-05-11 00:08:48 +0200

    Add adminpanel changes

---
+++ app/src/main/java/cookbook/backend/be_controllers/ModifyUserController.java
@@ -0,0 +1,43 @@
+package cookbook.backend.be_controllers;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import cookbook.backend.DatabaseMng;
+
+public class ModifyUserController {
+    
+    private DatabaseMng dbManager;
+    
+    public ModifyUserController(DatabaseMng dbManager) {
+        this.dbManager = dbManager; // Assign the passed instance to the dbManager field
+    }
+
+    public boolean modifyUser(Long userId, String userName, String displayName, String password) {
+        String sql = "UPDATE users SET UserName = ?, DisplayName = ?, Password = ? WHERE UserID = ?";
+        try (PreparedStatement statement = dbManager.getConnection().prepareStatement(sql)) {
+            statement.setString(1, userName);
+            statement.setString(2, displayName);
+            statement.setString(3, password);
+            statement.setLong(4, userId);
+
+            int result = statement.executeUpdate();
+            return result > 0;
+        } catch (SQLException e) {
+            System.out.println("Error updating user: " + e.getMessage());
+            return false;
+        }
+    }
+
+  public boolean deleteUser(Long userId, String userName, String displayName, String password) {
+		String sql = "DELETE FROM users WHERE UserID = ?;";
+		try (PreparedStatement statement = dbManager.getConnection().prepareStatement(sql)) {
+				statement.setLong(1, userId);
+				statement.executeUpdate();
+				return true;
+		} catch (SQLException e) {
+				System.out.println("Error updating user: " + e.getMessage());
+				return false;
+		}
+	}
+}
---
+++ app/src/main/java/cookbook/frontend/fe_controllers/ModifyUserViewController.java
@@ -0,0 +1,196 @@
+package cookbook.frontend.fe_controllers;
+
+import cookbook.backend.be_controllers.ModifyUserController;
+import cookbook.backend.be_controllers.UserController;
+import cookbook.backend.be_objects.User;
+import cookbook.backend.be_objects.LogIn;
+import cookbook.backend.DatabaseMng;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.collections.FXCollections;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ModifyUserViewController {
+    
+    // All the fields
+    @FXML
+    private TextField newPasswordField;
+    
+    @FXML
+    private ComboBox<User> userComboBox;
+    
+    @FXML
+    private TextField newUsernameField;
+
+    @FXML
+    private TextField newDisplayNameField;
+
+    @FXML
+    private ProgressIndicator progressCircle;
+    
+    @FXML
+    private Label errorLabel;
+    
+    @FXML
+    private Button submitButton;
+    
+    // Reference the UserController and the Database
+    private UserController userController;
+    private DatabaseMng dbManager;
+    private ModifyUserController modifyUserController;
+    private LogIn login;
+    
+    public ModifyUserViewController() throws SQLException {
+        this.dbManager = new DatabaseMng();
+        this.userController = new UserController(dbManager);
+        this.modifyUserController = new ModifyUserController(dbManager);
+        this.login = new LogIn(dbManager);
+    }
+    
+    // Method to load all users to the dropdown combobox.
+    private void loadUsers() throws SQLException {
+        List<User> users = userController.getAllUsers();
+        userComboBox.setItems(FXCollections.observableArrayList(users));
+        userComboBox.setConverter(new StringConverter<User>() {
+            @Override
+            public java.lang.String toString(User user) {
+                return (user != null) ? user.getUserName() : "Select user";
+            }
+    
+            @Override
+            public User fromString(String string) {
+                return null;
+            }
+        });
+    }
+  
+		@FXML
+		private void deleteUser(ActionEvent event) {
+			errorLabel.setText("");
+			progressCircle.setVisible(true);
+
+			User selectUser = userComboBox.getValue();
+
+			if (selectUser != null) {
+				Task<Boolean> deleteTask = new Task<>() {
+					protected Boolean call() throws Exception {
+						return modifyUserController.deleteUser(selectUser.getUserId(), selectUser.getUserName(), selectUser.getDisplayName(), selectUser.getPasswordHash());
+					}
+				};
+
+				deleteTask.setOnSucceeded(e -> {
+					progressCircle.setVisible(false);
+					if (deleteTask.getValue()) {
+						errorLabel.setTextFill(Color.GREEN);
+						errorLabel.setText("Deletion successful!");
+					} else {
+							errorLabel.setTextFill(Color.RED);
+							errorLabel.setText("Deletion Failed.");
+					}
+				});
+
+				deleteTask.setOnFailed(e -> {
+					progressCircle.setVisible(false);
+					errorLabel.setTextFill(Color.RED);
+					errorLabel.setText("Error during deletion.");
+				});
+
+				new Thread(deleteTask).start();
+			} else {
+					progressCircle.setVisible(false);
+					errorLabel.setTextFill(Color.RED);
+					errorLabel.setText("No user selected");
+			}
+		}
+    
+    @FXML
+    private void submitUserChanges(ActionEvent event) {
+        // Error label and progress bar
+        errorLabel.setText("");
+        progressCircle.setVisible(true);
+        
+        // Get the input from the admin.
+        String username = newUsernameField.getText();
+        String displayname = newDisplayNameField.getText();
+        String password = newPasswordField.getText();
+        User selectedUser = userComboBox.getValue();
+
+        String hashedPassword = login.hashPassword(password);
+        
+        // Boolean that initializes a new Task object
+        if (selectedUser != null) {
+            Task<Boolean> modifyTask = new Task<>() {
+                @Override
+                protected Boolean call() throws Exception {
+                    return modifyUserController.modifyUser(selectedUser.getUserId(), username, displayname, hashedPassword);
+                }
+            };
+            
+            // If task succeeds
+            modifyTask.setOnSucceeded(e -> {
+                progressCircle.setVisible(false);
+                if (modifyTask.getValue()) {
+                    errorLabel.setTextFill(Color.GREEN);
+                    errorLabel.setText("Modification Successful!");
+                } else {
+                    errorLabel.setTextFill(Color.RED);
+                    errorLabel.setText("Modification Failed.");
+                }
+            });
+
+            // If task fails
+            modifyTask.setOnFailed(e -> {
+                progressCircle.setVisible(false);
+                errorLabel.setTextFill(Color.RED);
+                errorLabel.setText("Error during modification.");
+            });
+
+            new Thread(modifyTask).start();
+        } else {
+            progressCircle.setVisible(false);
+            errorLabel.setTextFill(Color.RED);
+            errorLabel.setText("No user selected.");
+        }
+    }
+    
+    @FXML
+    public void initialize() throws SQLException {
+        try {
+            loadUsers();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+  @FXML
+  private void handleBackButton (ActionEvent event) {
+    try {
+      //Load the registration page FXML
+      Parent registrationPageParent = FXMLLoader.load(getClass().getResource("/AdminPanelView.fxml"));
+      Scene registrationPageScene = new Scene(registrationPageParent);
+
+      // Get the current stage and replace it
+      Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+      window.setScene(registrationPageScene);
+      window.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
