07ebb08e449bd935b5d36d0d65f86eeba9bb724b
Firas Moussa <fm222us@student.lnu.se>
2024-05-12 01:13:43 +0200

    Add app icon

--- app/src/main/java/cookbook/frontend/UIHandler.java
+++ app/src/main/java/cookbook/frontend/UIHandler.java
@@ -6,30 +6,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.text.Font;
 
 public class UIHandler extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    // Loading all fonts
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSText-Regular.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Black.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Bold.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Heavy.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Light.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Medium.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Regular.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Semibold.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Thin.otf").toExternalForm(), 10);
-    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Ultralight.otf").toExternalForm(), 10);
     // Load the FXML layout
     Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
 
-    // Set the application icon
-    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
+    // Load and set multiple icons
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_16x16.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_32x32.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_64x64.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_128x128.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_256x256.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_512x512.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_1024x1024.png")));
 
-    // Set the scene and show the stage
+
+    // Rest of your code to set up the scene, etc.  
     primaryStage.setScene(new Scene(root, 934, 703));
     primaryStage.setTitle("Cookbook");
     primaryStage.show();
