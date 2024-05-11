9f517efe8c400fb6161315c7e43b136eabcded5e
Firas Moussa <fm222us@student.lnu.se>
2024-05-12 00:40:23 +0200

    Add fonts

--- app/src/main/java/cookbook/frontend/UIHandler.java
+++ app/src/main/java/cookbook/frontend/UIHandler.java
@@ -6,11 +6,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 public class UIHandler extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    // Loading all fonts
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSText-Regular.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Black.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Bold.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Heavy.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Light.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Medium.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Regular.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Semibold.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Thin.otf").toExternalForm(), 10);
+    Font.loadFont(UIHandler.class.getResource("/fonts/SFNSDisplay-Ultralight.otf").toExternalForm(), 10);
     // Load the FXML layout
     Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
 
