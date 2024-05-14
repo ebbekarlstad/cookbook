109b64242afaffe1c0e1a387c8360f9273a3726f
Kinan Almasri <ka223mf@student.lnu.se>
2024-05-15 00:48:56 +0200

    created NavigationViewAdminController

---
+++ app/src/main/java/cookbook/frontend/fe_controllers/NavigationViewAdminController.java
@@ -0,0 +1,18 @@
+package cookbook.frontend.fe_controllers;
+
+import cookbook.backend.be_objects.Recipe;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class NavigationViewAdminController {
+
+   
+}
