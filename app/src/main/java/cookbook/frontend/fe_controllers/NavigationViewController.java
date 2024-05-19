33f6f9475eaccd0b4e484eda38b4923815574736
rd222ki <rd222ki@student.lnu.se>
2024-05-19 14:11:48 +0200

    Added alert for unopened messages in navigationview

--- app/src/main/java/cookbook/frontend/fe_controllers/NavigationViewController.java
+++ app/src/main/java/cookbook/frontend/fe_controllers/NavigationViewController.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import cookbook.backend.DatabaseMng;
+import cookbook.backend.be_controllers.MessageController;
 import cookbook.backend.be_objects.UserSession;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
@@ -34,11 +37,41 @@ public class NavigationViewController {
   @FXML
   private Label userDisplayName;
 
+  private MessageController messageController;
+
   @FXML
   private void initialize() throws SQLException {
     DatabaseMng dbManager = new DatabaseMng();
     userDisplayName.setTextFill(Color.WHITE);
     userDisplayName.setText("Welcome, " + dbManager.getDisplayName(UserSession.getInstance().getUserId()) + "!");
+
+    messageController = new MessageController(dbManager);
+
+    Platform.runLater(() -> {
+      checkForUnreadMessages();
+    });
+  }
+
+  private void checkForUnreadMessages() {
+    UserSession userSession = UserSession.getInstance();
+    if (!userSession.isUnreadMessagesChecked()) {
+      try {
+        Long userId = userSession.getUserId();
+        int unreadMessagesCount = messageController.getUnreadMessagesCount(userId);
+
+        if (unreadMessagesCount > 0) {
+          Alert alert = new Alert(Alert.AlertType.INFORMATION);
+          alert.setTitle("Unread Messages");
+          alert.setHeaderText(null);
+          alert.setContentText("You have " + unreadMessagesCount + " unread message(s).");
+          alert.showAndWait();
+        }
+
+        userSession.setUnreadMessagesChecked(true);
+      } catch (SQLException e) {
+        System.err.println("Failed to check for unread messages: " + e.getMessage());
+      }
+    }
   }
 
   @FXML
