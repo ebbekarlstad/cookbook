f79374ef9b408a702c0ae5f610bb9056f37d7869
Anas Adiel <aa226ws@student.lnu.se>
2024-05-19 21:51:59 +0200

    Fixed the issues with the icons inside of mac dock

--- app/src/main/java/cookbook/frontend/UIHandler.java
+++ app/src/main/java/cookbook/frontend/UIHandler.java
@@ -8,6 +8,10 @@ import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.Taskbar.Feature;
+
+
 public class UIHandler extends Application {
 
   @Override
@@ -25,6 +29,19 @@ public class UIHandler extends Application {
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_256x256.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_512x512.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_1024x1024.png")));
+
+
+    //Set icon on the taskbar/dock
+    if (Taskbar.isTaskbarSupported()) {
+      var taskbar = Taskbar.getTaskbar();
+
+      if (taskbar.isSupported(Feature.ICON_IMAGE)) {
+        final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        var dockIcon = defaultToolkit.getImage(getClass().getResource("/images/icon.png"));
+        taskbar.setIconImage(dockIcon);
+      }
+
+    }
 
     // Rest of your code to set up the scene, etc.  
     primaryStage.setScene(new Scene(root, 934, 703));
