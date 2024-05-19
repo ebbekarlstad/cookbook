cbb536247419eb6060edffbeae43eb0276e5ab08
Firas Moussa <fm222us@student.lnu.se>
2024-05-19 22:22:41 +0200

    Update app icon

--- app/src/main/java/cookbook/frontend/UIHandler.java
+++ app/src/main/java/cookbook/frontend/UIHandler.java
@@ -22,14 +22,15 @@ public class UIHandler extends Application {
     Parent root = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
 
     // Load and set multiple icons
-    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_16x16.png")));
+/*     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_16x16.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_32x32.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_64x64.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_128x128.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_256x256.png")));
     primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_512x512.png")));
-    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_1024x1024.png")));
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/app_icon_1024x1024.png"))); */
 
+    primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
 
     //Set icon on the taskbar/dock
     if (Taskbar.isTaskbarSupported()) {
